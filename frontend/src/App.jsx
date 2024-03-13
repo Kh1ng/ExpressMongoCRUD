1038e5eabb78e46679d8401f29e7e2fa191c7ffc
Colton Spurgin <khing@pm.me>
2024-03-12 19:03:34 -0500

    query client, routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,27 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: Infinity,
+      cacheTime: Infinity,
+    },
+  },
+});
 import "./App.css";
 
 function App() {
-  return <div className="App">What the crud</div>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <h1> NEAT-O </h1>
+        <Routes>
+          <Route path="/details/" element={<div> insert CRUD here </div>} />
+          <Route path="/" element={<div> ROOT TOOTY </div>} />
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
 }
 
 export default App;
